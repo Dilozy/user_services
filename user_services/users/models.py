9c97a512c2605ab1d97cfa1abcabc428310273f0
dilozy <denfil228@yandex.ru>
2025-06-10 14:47:20 +0300

    Finished custom usr

--- user_services/users/models.py
+++ user_services/users/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 
-from .manager import CustomUserManager
-
 
 class CustomUser(AbstractUser):
     phone_regex = RegexValidator(regex=r"^((\+7)|8)\d{10}$",
@@ -14,13 +12,3 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=12, unique=True,
                              validators=[phone_regex],
                              )
-    objects = CustomUserManager()
-    USERNAME_FIELD = "phone"
-    REQUIRED_FIELDS = ["email"]
-
-    @property
-    def username(self):
-        return self.phone
-
-    def __str__(self):
-        return {self.username}

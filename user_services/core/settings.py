9b77731e7b7548bf58fcb32640cb2f6a23271fe8
dilozy <denfil228@yandex.ru>
2025-06-14 16:12:03 +0300

    Project is finished

--- user_services/core/settings.py
+++ user_services/core/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uaqra5wz97b^@1^-uq2f!ucxge*97^9t!4x_f6_0h8okgs$(@@'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
--- user_services/products/views.py
+++ user_services/products/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Prefetch
 
-from .models import Order
+from .models import Order, OrderItem
 from .serializers import OrderSerializer
 from subscriptions.models import UserSubscription
 
@@ -17,8 +17,21 @@ class OrderViewSet(viewsets.ModelViewSet):
         return context
     
     def get_queryset(self):
-        return Order.objects.all().select_related("user__subscription__tariff") \
-                                  .prefetch_related("items",
-                                                    "items__product") \
-                                  .filter(user=self.request.user) \
+        return Order.objects.select_related("user__subscription__tariff") \
+                            .prefetch_related(
+                                Prefetch(
+                                    "items",
+                                    queryset=OrderItem.objects.select_related("product")
+                                    )) \
+                            .filter(user=self.request.user) \
+                            .defer("user__last_login", "user__password",
+                                   "user__first_name", "user__last_name",
+                                   "user__is_superuser", "user__username", "user__email",
+                                   "user__is_staff", "user__is_active", "user__date_joined",
+                                   "user__phone", "user__telegram_chat_id",
+                                   "user__subscription__start_date",
+                                   "user__subscription__end_date",
+                                   "user__subscription__tariff__name",
+                                   "user__subscription__tariff__duration_in_days",
+                                   "user__subscription__tariff__description")
 

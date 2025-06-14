4b444c6e1176ce0cfd22c82903a9e9dad7c27e23
dilozy <denfil228@yandex.ru>
2025-06-14 22:27:52 +0300

    Refactored a few files

--- user_services/products/views.py
+++ user_services/products/views.py
@@ -20,27 +20,31 @@ class OrderViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         total_price = Sum(F("items__product__price") * F("items__quantity"))
         
-        discount_price_query = ExpressionWrapper(
+        discount_price = ExpressionWrapper(
             total_price - 
             total_price * F("user__subscription__tariff__discount_percent") /
             Decimal(100),
             output_field=DecimalField(max_digits=10, decimal_places=2)
             )
         
+        unused_fields = ("user__last_login", "user__password",
+                         "user__first_name", "user__last_name",
+                         "user__is_superuser", "user__username", "user__email",
+                         "user__is_staff", "user__is_active", "user__date_joined",
+                         "user__phone",
+                         "user__subscription__start_date",
+                         "user__subscription__end_date",
+                         "user__subscription__tariff__name",
+                         "user__subscription__tariff__duration_in_days",
+                         "user__subscription__tariff__description")
+        
         return Order.objects.select_related("user__subscription__tariff") \
-                            .prefetch_related(Prefetch(
+                            .prefetch_related(
+                                Prefetch(
                                     "items",
-                                    queryset=OrderItem.objects.select_related("product"))
-                                    ) \
+                                    queryset=OrderItem.objects.select_related("product")
+                                    )
+                                ) \
                             .filter(user=self.request.user) \
-                            .annotate(total_price=discount_price_query) \
-                            .defer("user__last_login", "user__password",
-                                   "user__first_name", "user__last_name",
-                                   "user__is_superuser", "user__username", "user__email",
-                                   "user__is_staff", "user__is_active", "user__date_joined",
-                                   "user__phone",
-                                   "user__subscription__start_date",
-                                   "user__subscription__end_date",
-                                   "user__subscription__tariff__name",
-                                   "user__subscription__tariff__duration_in_days",
-                                   "user__subscription__tariff__description")
+                            .annotate(total_price=discount_price) \
+                            .defer(*unused_fields)
